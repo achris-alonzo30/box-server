8d82e033565ef3c8ca89e0c85b09bd4405bf8bf0
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-30 20:41:07 -0300

    install packages for setting up server and add the boiler plate code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+
+/* ----------------------------- CONFIGURATIONS ----------------------------- */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* --------------------------------- ROUTES --------------------------------- */
+
+
+/* --------------------------------- SERVER --------------------------------- */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port} 🚀`);})
